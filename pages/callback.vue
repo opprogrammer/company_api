e1485d9adf658c7831fb8aa51584895ec258cd2a
opprogrammer <97902777+opprogrammer@users.noreply.github.com>
2023-06-23 17:29:22 +0530

    Add files via upload

---
+++ pages/callback.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Please Wait</h1>
+	</div>
+</template>
+<script setup>
+	const { Realm } = useMyRealmApp();
+
+	onMounted(() => {
+		Realm.handleAuthRedirect();
+	});
+</script>
